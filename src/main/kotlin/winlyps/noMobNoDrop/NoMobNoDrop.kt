e2a6b30ebae5071f444682a4f39fba6282787367
Winlypse <nts3@o2.pl>
2024-10-22 14:35:26 +0200

    Initial commit

---
+++ src/main/kotlin/winlyps/noMobNoDrop/NoMobNoDrop.kt
@@ -0,0 +1,14 @@
+package winlyps.noMobNoDrop
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class NoMobNoDrop : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
